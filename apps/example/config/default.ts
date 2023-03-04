ad830dbcffa0ed7a6ceaefa91e0c36e58c39efe6
Andrzej Pasterczyk <andrzej.pasterczyk+github@gmail.com>
2023-03-04 22:05:54 +0100

    chore: added example config

---
+++ apps/example/config/default.ts
@@ -0,0 +1,9 @@
+import type * as Stuntman from '@stuntman/shared';
+
+const config: Stuntman.RecursivePartial<Stuntman.Config> = {
+    mock: {
+        timeout: 20000
+    }
+};
+
+module.exports = config;
