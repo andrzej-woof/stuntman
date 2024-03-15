e05d8a9432ed060a78f8da39a4b81d7decc88b56
Adrien Lenoir <adrien.lenoir42440@gmail.com>
2024-03-15 17:01:41 +0100

    Dependencies upgrade (#5)
    * chore: format unformatted files
    
    * chore: upgrade lockfile version
    
    * chore: upgrade eslint
    
    * fix: eslint issues following the upgrade
    
    * chore: upgrade typescript to 5.4.2
    
    * fix: issues following TS upgrade to v5
    
    * chore: upgrade jest and ts-jest to suppress TS warnings
    
    * chore: upgrade pnpm version in CI

--- apps/example/config/default.ts
+++ apps/example/config/default.ts
@@ -2,8 +2,8 @@ import type * as Stuntman from '@stuntman/shared';
 
 const config: Stuntman.RecursivePartial<Stuntman.Config> = {
     mock: {
-        timeout: 20000
-    }
+        timeout: 20000,
+    },
 };
 
 module.exports = config;
--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,9 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  coverageProvider: 'v8',
-  testPathIgnorePatterns: ['/node_modules/', '/__rules/', '/.cache/'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/.cache/']
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    coverageProvider: 'v8',
+    testPathIgnorePatterns: ['/node_modules/', '/__rules/', '/.cache/'],
+    coveragePathIgnorePatterns: ['/node_modules/', '/.cache/'],
 };
